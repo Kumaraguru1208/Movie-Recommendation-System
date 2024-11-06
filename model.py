d1e594011ae92fc28218046cdc035cdf35e9e46d
Kumaraguru1208 <141935541+Kumaraguru1208@users.noreply.github.com>
2024-11-06 18:00:26 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,31 +5,23 @@ from sklearn.metrics.pairwise import linear_kernel
 
 app = Flask(__name__)
 
-# Load the dataset
 file_path = 'imdb-movies-dataset.csv'
 movies_df = pd.read_csv(file_path)
-
-# Fill NaN values in relevant columns with an empty string
 for column in ['Genre', 'Title' , 'Director']:
     movies_df[column] = movies_df[column].fillna('')
 
-# Create a 'soup' column
+
 movies_df['soup'] = (
         movies_df['Genre'] + ' ' +
         movies_df['Title'] + ' ' +
         movies_df['Director']
 )
-
-# Calculate TF-IDF and cosine similarity
 tfidf = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfidf.fit_transform(movies_df['soup'])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-# Create a mapping of movie titles to indices
 indices = pd.Series(movies_df.index, index=movies_df['Title']).drop_duplicates()
 
-
-# Recommendation function
 def get_recommendations(title, cosine_sim=cosine_sim, df=movies_df, indices=indices):
     if title not in indices:
         return ["Movie not found. Please try another title."]
@@ -42,8 +34,6 @@ def get_recommendations(title, cosine_sim=cosine_sim, df=movies_df, indices=indi
     movie_indices = [i[0] for i in sim_scores]
     return df['Title'].iloc[movie_indices].tolist()
 
-
-# Define the main route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     recommendations = []
